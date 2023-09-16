b62f3340847a636c4a5d75cdce3d4ee71c8cd220
mihirh19 <miheerhadvani990@gmail.com>
2023-09-16 15:43:34 +0530

    vercel added

--- backend/cfehome/settings.py
+++ backend/cfehome/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = "django-insecure-m^*ivj*78adi@6&l*4+fz2*m88nhl7zcacx%!fvfr-0i$ncf$!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '.vercel.app',
+    '127.0.0.1',
+    'localhost',
+]
 
 
 # Application definition

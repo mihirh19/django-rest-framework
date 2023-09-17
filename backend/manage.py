4e1969820462beed238c9f40cd1f9d82081ed226
mihirh19 <miheerhadvani990@gmail.com>
2023-09-17 10:29:19 +0530

    update env

--- backend/manage.py
+++ backend/manage.py
@@ -2,10 +2,10 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-import dotenv
+import environ
 
 def main():
-    dotenv.load_dotenv()
+    environ.Env.read_env()
     """Run administrative tasks."""
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "cfehome.settings")
     try:
